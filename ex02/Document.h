4ca17ca275caae58e0f7fecf7439afc83ffaa005
Adiel Matuf <matufadiel@gmail.com>
2019-05-17 19:46:51 +0000

    init ex03

--- ex02/Document.h
+++ ex02/Document.h
@@ -22,20 +22,20 @@ private:
 public:
 	Document();
 	~Document();
-  void print_current() const; //prints the current line (ed maintains a current line)
+    void print_current() const; //prints the current line (ed maintains a current line)
 	void current_info() const; //prints line number of current line followed by TAB followed by current line
 	void print_all() const; //prints all lines
-  void change_current(int); // makes line #7 the current line
-  void append_after(const string&); // appends new text after the current line
-  void insert_before(const string&); // inserts new text before the current line
-  void change_line(const string&); // changes the current line for text that follows
-  void delete_current(); // deletes the current line
-  int search(const string&); // searches forward after current line for the specified text. 
+    void change_current(int); // makes line #7 the current line
+    void append_after(const string&); // appends new text after the current line
+    void insert_before(const string&); // inserts new text before the current line
+    void change_line(const string&); // changes the current line for text that follows
+    void delete_current(); // deletes the current line
+    int search(const string&); // searches forward after current line for the specified text. 
                   // The search wraps to the beginning of the buffer and continues down to the current line, 
                   //if necessary
-  void replace(string&, string&); //replaces old string with new in current line (google: C++ split or token)
-  void quit(); //Quits the editor without 
+    void replace(string&, string&); //replaces old string with new in current line (google: C++ split or token)
+    void quit(); //Quits the editor without 
   
-  int get_current_line() const;
-  void set_current_line(int);
+    int get_current_line() const;
+    void set_current_line(int);
 };
